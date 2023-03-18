997382c72b7cf1e6db3046935590a04f32bbd06d
Abdul Rasheed N S <alrasheedtholicode@gmail.com>
2023-03-18 15:09:50 +0400

    env added

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
-import dotenv from 'dotenv'
-dotenv.config()
+require('dotenv').config();
 const MONGO_PASS = process.env.MONGO_PASS;
 const MONGO_USERNAME  = process.env.MONGO_USERNAME ;
 const MONGO_URI = process.env.MONGO_URI 
+
 const mongoClient=require('mongodb').MongoClient
 const state={
     db:null
 }
-
 module.exports.connect=function(done){
     const url=MONGO_URI
     // const url='mongodb://0.0.0.0:27017'
